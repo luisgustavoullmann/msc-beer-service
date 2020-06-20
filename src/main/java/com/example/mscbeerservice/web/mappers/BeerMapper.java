f8c6b89392f05b1c0fb64f7584d9bffab25eac9f
luisgustavoullmann <luisgustavoullmann@gmail.com>
2020-06-20 10:33:11 -0300

    Added BeerMapper/DateMapper. Closes #10

---
+++ src/main/java/com/example/mscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,15 @@
+package com.example.mscbeerservice.web.mappers;
+
+import com.example.mscbeerservice.domain.Beer;
+import com.example.mscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Luis Gustavo Ullmann on 20/06/2020
+ */
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto BeerToDeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
