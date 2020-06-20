27efed2c6464b04226e592ef636a060edcd536b7
luisgustavoullmann <luisgustavoullmann@gmail.com>
2020-06-20 10:04:40 -0300

    Added MVC Error Handler - BeerDto. Closes #9

--- src/main/java/com/example/mscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/example/mscbeerservice/web/controller/BeerController.java
@@ -4,6 +4,7 @@ package com.example.mscbeerservice.web.controller;
 import com.example.mscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -23,13 +24,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
