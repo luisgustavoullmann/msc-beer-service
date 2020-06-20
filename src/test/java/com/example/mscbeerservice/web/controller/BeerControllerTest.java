000e6e5339e3e5f86167d6c2dee8d12e36f478d4
luisgustavoullmann <luisgustavoullmann@gmail.com>
2020-06-20 10:43:55 -0300

    Fixing Test. Closes #11

--- src/test/java/com/example/mscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/example/mscbeerservice/web/controller/BeerControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -33,23 +34,32 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(beerDtoJson))
-                .andExpect(status().isCreated());
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(beerDtoJson))
+            .andExpect(status().isCreated());
     }
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        return BeerDto.builder()
+                .beerName("Whatever")
+                .beerStyle("Bad")
+                .price(new BigDecimal("3.50"))
+                .upc(1212121212121212L)
+                .build();
     }
 }
